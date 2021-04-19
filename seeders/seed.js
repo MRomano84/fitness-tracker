acc0f8d56dd9cb0edd92e41b26417369c82ef859
Matthew Romano <mathiasrok@gmail.com>
2021-04-19 18:00:58 -0400

    db connection

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,9 @@
 let mongoose = require("mongoose");
 let db = require("../models");
+const path = require("path");
+require('dotenv').config({path: path.join(__dirname, "..", ".env")});
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config();
+const path = require("path");
+require('dotenv').config({path: path.join(__dirname, "..", ".env")});
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 const app = express();
 
@@ -11,15 +12,30 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-console.log(process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+console.log(process.env.URI);
+mongoose.connect(process.env.URI || "mongodb://localhost/workoutDB", {
+    useUnifiedTopology: true,
     useNewUrlParser: true,
-    useFindAndModify: false
-});
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => {
+    console.log('Connection estabislished with MongoDB');
+})
+.catch(error => console.error(error.message));
 
 // routes
 app.use(require("./routes/api.js"));
+// will need html routes
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://myMongo:myMongoUNH@cluster0.0ylfj.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
